3831c048c598fe64bd135be0e474d20ca74abb2d
dd86k <dd@dax.moe>
2024-04-07 09:22:04 -0400

    Update types, add .gitignore

--- types.d
+++ types.d
@@ -1,21 +1,47 @@
 import std.stdio;
 
+//TODO: Redo with array of TypeInfo (typeid(T))
+
+immutable int padding = 10;
+
+void printin(T)(string altname = null)
+{
+    writefln("%*s  init=%s\tmin=%s\tmax=%s",
+        padding, altname ? altname : T.stringof,
+        T.init, T.min, T.max);
+}
+void printch(T)()
+{
+    writefln("%*s  init=%u\tmin=%u\tmax=%u",
+        padding, T.stringof,
+        T.init, T.min, T.max);
+}
+void printfp(T)()
+{
+    writefln("%*s  dig=%s\tmant_dig=%s\tepsilon=%s\tmin_normal=%s\tmax=%s\tinit=%s",
+        padding, T.stringof,
+        T.dig, T.mant_dig, T.epsilon, T.min_normal, T.max, T.init);
+}
+
 void main()
 {
-    writeln("byte.min   : ", byte.min);
-    writeln("byte.max   : ", byte.max);
-    writeln("ubyte.min  : ", ubyte.min);
-    writeln("ubyte.max  : ", ubyte.max);
-    writeln("short.min  : ", short.min);
-    writeln("short.max  : ", short.max);
-    writeln("ushort.min : ", ushort.min);
-    writeln("ushort.max : ", ushort.max);
-    writeln("int.min    : ", int.min);
-    writeln("int.max    : ", int.max);
-    writeln("uint.min   : ", uint.min);
-    writeln("uint.max   : ", uint.max);
-    writeln("long.min   : ", long.min);
-    writeln("long.max   : ", long.max);
-    writeln("ulong.min  : ", ulong.min);
-    writeln("ulong.max  : ", ulong.max);
+    // Integrals
+    printin!byte;
+    printin!ubyte;
+    printin!short;
+    printin!ushort;
+    printin!int;
+    printin!uint;
+    printin!long;
+    printin!ulong;
+    printin!size_t("size_t");
+    printin!ptrdiff_t("ptrdiff_t");
+    // Characters
+    printch!char;
+    printch!wchar;
+    printch!dchar;
+    // Floats
+    printfp!float;
+    printfp!double;
+    printfp!real; // NOTE: Still uses x87 types
 }
