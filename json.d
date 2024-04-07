8fc1fc779dde4d47c4bd103bf03ed4a455b867e3
dd86k <dd@dax.moe>
2024-04-07 17:14:07 -0400

    json: Add manual array build

--- json.d
+++ json.d
@@ -5,11 +5,44 @@ import std.json;
 
 void main()
 {
-    JSONValue jroot;
+    JSONValue j;
     
-    JSONValue jputer = ["iscool":true];
+    // Associative array
+    j["puter"] = ["iscool":true];
     
-    jroot["puter"] = jputer;
+    // Build array manually
+    struct F { char[20] f; size_t fl; char[20] v; size_t vl; }
+    struct S { size_t c; F[5] f; }
+    static immutable S[] data = [
+        {
+            2,
+            [
+            { "field", 5, "value", 5 },
+            { "say",   3, "what!", 5 },
+            ]
+        },
+        {
+            3,
+            [
+            { "ext",    3, "1100", 4 },
+            { "name",   4, "John", 4 },
+            { "secret", 6, "iloveD2001", 10 },
+            ]
+        },
+    ];
+    string[string][] list = new string[string][data.length];
+    size_t i;
+    foreach (item; data) // foreach items
+    {
+        foreach (ref row; item.f[0..item.c])
+        {
+            string f = row.f[0..row.fl].idup;
+            string v = row.v[0..row.vl].idup;
+            list[i][f] = v;
+        }
+        ++i;
+    }
+    j["list"] = list;
     
-    writeln(jroot);
+    writeln(j);
 }
