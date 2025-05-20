c8d809d4fd59fed2758efbe4cb53e6eaf688061e
dd86k <dd@dax.moe>
2025-05-20 12:02:31 -0400

    aa: Include null

--- aa.d
+++ aa.d
@@ -6,11 +6,12 @@ void main(string[] args)
 {
     string[int] aa;
     
-    aa[50]  = "test";
+    aa[50] = "test";
     aa[86] = "bird";
+    aa[22] = null; // null can be included
     
     foreach (key, val; aa)
     {
-        writeln("key: ", key, ", value: ", val);
+        writeln("key: ", key, ", value: ", val ? val : "(null)");
     }
 }
